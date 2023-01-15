c34d49b0ff1b2e88590a66674d2efd7397ad9e8e
anjanasainju <anjanasainju@gmail.com>
2023-01-15 15:39:43 -0500

    added search bar

--- my-app/App.js
+++ my-app/App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
+import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Let's make the grocery list!</Text>
+    <View style={styles.background}>
+      <View style={styles.searchbar}>
+        <View>
+        <TextInput
+          placeholder ="Search..."
+        />
+        </View>
+        <Ionicons name="search-outline" size={20} color={"#fff"} justify />
+        
+       
+      
+      <ScrollView>
+        
+      </ScrollView>
       <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
+  },
+  searchbar: {
+    flexDirection:'row',
+    backgroundColor: "#999",
+    padding: 10,
+    height: 50,
+    width: "90%",
+    borderRadius: 8,
+    marginBottom: 15,
+    marginTop: 50,
   },
 });
