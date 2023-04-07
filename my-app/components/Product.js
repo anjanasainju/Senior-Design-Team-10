c380e12131b301989c1b93dc7a53f434e5b2719c
anjanasainju <anjanasainju@gmail.com>
2023-04-07 16:55:11 -0400

    delete?

--- my-app/components/Product.js
+++ my-app/components/Product.js
@@ -1,72 +1,66 @@
-import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from "react-native";
-import Ionicons from '@expo/vector-icons/Ionicons';
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
+// const deleteProducts = (index) => {
+//   prod.splice(index, 1);
+// };
 const Product = (props) => {
+  //   const prod = props.products;
+  //   console.log(prod);
 
-    return(
-        <View style={styles.item}>
-            <View style={styles.itemLeft}>
-                <Text style={styles.itemText}>{props.text}</Text>
-            </View>
-            {/*Quantity */}
-            <View style={styles.itemRight}>
-            {/* <TouchableOpacity>
-                <View style={styles.quanitityWrapper}>  */}
-                {/*make different styles for plus, minus and whole right section*/}
-                    {/* <Text style={styles.quantity}>-</Text>
-                </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-                <View style={styles.quanitityWrapper}>
-                   
-                    <Text style={styles.quantity}>+</Text>
-                </View>
-            </TouchableOpacity> */}
-            <Ionicons name="trash-outline" size={28} color="red" />
-            </View>
-            
-        </View>
-    )
-}
+  return (
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <Text style={styles.itemText}>{props.text}</Text>
+      </View>
+      <View style={styles.itemRight}>
+        <Ionicons
+          name="trash-outline"
+          size={28}
+          color="red"
+          //   onPress={() => deleteProducts()}
+        />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    item: {
-        backgroundColor:'#FFF',
-        padding: 15,
-        borderRadius: 10,
-        flexDirection:'row',
-        alignItems:'center',
-        justifyContent: 'space-between',
-        marginBottom: 20,
-        marginLeft:20,
-        marginRight:20
-
-    },
-    itemLeft: {
-        flexDirection:'row',
-        alignItems:'center',
-        flexWrap: 'wrap',
-    },
-    itemText: {
-        maxWidth: '100%'
-
-    },
-    itemRight:{
-        flexDirection:'row',
-    },
-    quanitityWrapper: {
-        width: 24,
-        height: 24,
-        borderColor: '#55BCF6',
-        borderWidth: 2,
-        borderRadius: 20,
-        justifyContent: 'center',
-        alignItems:'center',
-        marginLeft:20,
-    },
-    quantity: {
-        fontSize: 18
-    }
+  item: {
+    backgroundColor: "#FFF",
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  itemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  itemText: {
+    maxWidth: "100%",
+  },
+  itemRight: {
+    flexDirection: "row",
+  },
+  quanitityWrapper: {
+    width: 24,
+    height: 24,
+    borderColor: "#55BCF6",
+    borderWidth: 2,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 20,
+  },
+  quantity: {
+    fontSize: 18,
+  },
 });
 export default Product;
